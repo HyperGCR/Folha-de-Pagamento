3290e08ddfb1e833cef8e6bca6110dd2d293cd9c
Guilherme da Cunha Ribeiro <guilhermeriber342@gmail.com>
2026-06-04 18:45:07 -0300

    Add files via upload

---
+++ ColaboradorPadrao.java
@@ -0,0 +1,11 @@
+public class ColaboradorPadrao extends Colaborador{
+    
+    public ColaboradorPadrao(int numRegistro, String nomeComp){
+
+        super(numRegistro, nomeComp);
+
+
+    }
+
+
+}
